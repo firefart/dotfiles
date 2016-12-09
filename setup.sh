10017e15fe16d22eea365642d3e9a8cb39f92e95
Christian Mehlmauer <firefart@gmail.com>
2016-12-09 22:30:16 +0100

    more stuff

--- setup.sh
+++ setup.sh
@@ -2,6 +2,13 @@
 
 BASEDIR=$(dirname "$0")
 
+# tmux
 ln -s "$BASEDIR/tmux.conf" "~/.tmux.conf"
+
+# vim
+ln -s "$BASEDIR/vimrc" "~/.vimrc"
+
+# pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 
