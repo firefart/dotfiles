7822565ee7765344922af9e100b7c4b9c4d0fba7
Christian Mehlmauer <firefart@gmail.com>
2016-12-09 22:47:45 +0100

    more stuff

--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-BASEDIR=$(dirname "$0")
+BASEDIR=$(cd $(dirname "$1") && pwd -P)/$(basename "$1")
+
+echo "Basedir: $BASEDIR"
 
 # tmux
-ln -s "$BASEDIR/tmux.conf" "~/.tmux.conf"
+ln -fs $BASEDIR/tmux.conf ~/.tmux.conf
 
 # vim
-ln -s "$BASEDIR/vimrc" "~/.vimrc"
+ln -fs $BASEDIR/vimrc ~/.vimrc
 
 # pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+ln -fs $BASEDIR/bundle ~/.vim/
 
--- vimrc
+++ vimrc
@@ -2,3 +2,6 @@ execute pathogen#infect()
 syntax on
 filetype plugin indent on
 
+" lightline.vim configuration
+set laststatus=2
+
