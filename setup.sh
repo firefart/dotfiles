5658dfa7ffaf2b297f6c765cdd4413484a3d90c6
Christian Mehlmauer <firefart@gmail.com>
2016-12-09 23:20:31 +0100

    more stuff

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 BASEDIR=$(cd $(dirname "$1") && pwd -P)/$(basename "$1")
+# remove slash
+BASEDIR=${BASEDIR%/}
 
 echo "Basedir: $BASEDIR"
 
@@ -14,4 +16,6 @@ ln -fs $BASEDIR/vimrc ~/.vimrc
 mkdir -p ~/.vim/autoload && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 ln -fs $BASEDIR/bundle ~/.vim/
+mkdir -p ~/.vim/colors
+ln -fs $BASEDIR/colors/molokai/colors/molokai.vim ~/.vim/colors/molokai.vim
 
