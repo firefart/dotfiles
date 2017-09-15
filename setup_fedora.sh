130bb91ea693d2ce7eb3f29597b0d2237819d4ce
Christian Mehlmauer <firefart@gmail.com>
2017-09-15 23:19:31 +0200

    powerline

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -42,7 +42,6 @@ ln -fs $BASEDIR/vim/colors/molokai/colors/molokai.vim ~/.vim/colors/molokai.vim
 # powerline
 cd $BASEDIR/powerline-shell
 python ./setup.py install --user
-ln -fs $BASEDIR/powerline-shell/powerline-shell.py ~/.powerline-shell.py
 
 # powerline fonts
 mkdir -p ~/.local/share/fonts
